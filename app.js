de071a62c0391823d0f8b063fb7192c45bf03792
Aditya V <aditya.veerathu@gmail.com>
2022-07-24 10:41:54 -0400

    Changed js_files to js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const email = require("./js_files/email.js")
+const email = require("./js/email.js")
 
 email.sendMail().then((result) => console.log('Email sent...', result))
 .catch((error) => console.log(error.message));
